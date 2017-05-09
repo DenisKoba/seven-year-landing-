a4929e35993c37f78f83601b569d7b966e7ef97e
griffin <griffin@zeoalliance.com>
2017-05-09 21:58:14 +0300

    six commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
-/**
- * Created by griffin on 5/9/17.
- */
+$( document ).ready(function() {
+    var docHeight = $(".js-pattern").height(),
+        docWidth = $(".js-pattern").width(),
+        $div = $(".js-pattern-element"),
+        divWidth = $div.width(),
+        divHeight = $div.height(),
+        heightMax = docHeight - divHeight,
+        widthMax = docWidth - divWidth;
+
+    $div.css({
+        left: Math.floor( Math.random() * widthMax ),
+        top: Math.floor( Math.random() * heightMax )
+    });
+});
