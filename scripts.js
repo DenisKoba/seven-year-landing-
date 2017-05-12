ae5f82f3e93b7a2c522085124bf536795a4e66c6
griffin <griffin@zeoalliance.com>
2017-05-12 14:57:49 +0300

    eight commit

--- scripts.js
+++ scripts.js
@@ -1,14 +1,50 @@
 $( document ).ready(function() {
-    var docHeight = $(".js-pattern").height(),
-        docWidth = $(".js-pattern").width(),
-        $div = $(".js-pattern-element"),
-        divWidth = $div.width(),
-        divHeight = $div.height(),
-        heightMax = docHeight - divHeight,
-        widthMax = docWidth - divWidth;
 
-    $div.css({
-        left: Math.floor( Math.random() * widthMax ),
-        top: Math.floor( Math.random() * heightMax )
+var patternContainer = document.querySelector(".js-pattern");
+
+// добавляем слайды в блок UL
+class landingPattern{
+    constructor(item, name) {
+        this.item = item;
+        this.name = name;
+    }
+}
+
+var triangleWhite = new landingPattern();
+    triangleWhite.name = "triangle-white";
+    triangleWhite.item = [patternContainer.children[0]];
+
+var circleGray = new landingPattern();
+    circleGray.name = "circle-gray";
+    circleGray.item = [patternContainer.children[1]];
+
+
+    var circleOrange = new landingPattern();
+    circleOrange.name = "circle-orange";
+    circleOrange.item = [patternContainer.children[2]];
+
+    var squareWhite = new landingPattern();
+    squareWhite.name = "square-white";
+    squareWhite.item = [patternContainer.children[3]];
+
+var allItems = [triangleWhite, circleGray, circleOrange, squareWhite];
+
+allItems.forEach(function (i, item, arr){
+    for(i = 0; i < 3; i++){
+    patternElement = document.createElement("i");
+    patternElement.setAttribute("class", "js-pattern-element");
+    patternElement.className = patternElement.className + " " + arr[item].name;
+    patternContainer.appendChild(patternElement);
+    }
+    $(".js-pattern-element").each(function () {
+        var randomtop = Math.floor(Math.random() * 500);
+        var randomleft = Math.floor(Math.random() * 1000);
+        $(this).css({
+            "top": randomtop,
+            "left": randomleft
+        });
     });
 });
+
+});
+
