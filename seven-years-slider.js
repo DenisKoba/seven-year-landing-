2510467a86b0cb99880ac1e1770cee894b7d3fca
griffin <griffin@zeoalliance.com>
2017-05-22 14:41:34 +0300

    pre-final commit

--- seven-years-slider.js
+++ seven-years-slider.js
@@ -1,7 +1,7 @@
 
 
 
-function activateHexagons(n){
+/*function activateHexagons(n){
  var currentHexagon = $(n).attr('data-id');
  $target = $('.js-seven-years-slide-pager');
 
@@ -40,4 +40,41 @@ function activateHexagons(n){
  i++;
  }
  }, 4000);
- });
+ });*/
+
+var versionsLeft = document.querySelector("#js-seven-years-left-button");
+var versionsRight = document.querySelector("#js-seven-years-right-button");
+var sevenYearsPager = document.getElementsByClassName("js-seven-years-slide-pager");
+var sevenYearsSlide = document.getElementsByClassName("js-seven-years-slide");
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+ var i;
+ if (n > sevenYearsSlide.length) {slideIndex = 1}
+ if (n < 1) {slideIndex = sevenYearsSlide.length}
+ for (i = 0; i < sevenYearsSlide.length; i++) {
+  sevenYearsSlide[i].style.display = "none";
+ }
+ for (i = 0; i < sevenYearsPager.length; i++) {
+  sevenYearsPager[i].className = sevenYearsPager[i].className.replace(" js-seven-years-slide-pager-selected", " ");
+ }
+ sevenYearsSlide[slideIndex-1].style.display = "block";
+ sevenYearsPager[slideIndex-1].className += " js-seven-years-slide-pager-selected";
+}
+
+versionsLeft.onclick = function  () {
+ showSlides(slideIndex += -1);
+}
+
+
+versionsRight.onclick = function  () {
+ showSlides(slideIndex += 1);
+}
+
+
+function currentDiv(n) {
+ showSlides(slideIndex = n);
+}
