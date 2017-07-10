ca341976f7a86d755b67147af9b42adda3568bec
griffin <griffin@zeoalliance.com>
2017-07-10 17:52:43 +0300

    commit

--- seven-years-scripts.js
+++ seven-years-scripts.js
@@ -1,38 +1,85 @@
-$( document ).ready(function() {
-    // запускаю годы
-    $(".js-seven-years-dinamic-figure").fadeOut();
+// animation of years
 
-    function activateYears(n){
-        var currentYear = $(n).attr('data-id');
+function activateYears(n){
+    var currentYear = $(n).attr('data-id');
 
-        $('.js-year-text').hide();
-        $('.js-year-text[data-id="'+currentYear+'"]').fadeIn();
-        $('.js-year').removeClass('js-year-selected');
-        $(n).addClass('js-year-selected');
+    $('.js-year-text').hide();
+    $('.js-year-text[data-id="'+currentYear+'"]').fadeIn();
+    $('.js-year').removeClass('js-year-selected');
+    $(n).addClass('js-year-selected');
 
+}
+
+activateYears($('.js-year-text').first());
+
+$('.js-year').on('click', function(e) {
+    clearInterval(onClickSetTimeoutID);
+    activateYears(this);
+
+});
+
+$(function() {
+    var i = 0;
+    var $target = $('.js-year');
+    onClickSetTimeoutID = setInterval(function(){
+        activateYears($target.eq(i).addClass('js-year-selected'));
+        if ( i == $target.length - 1 ) {
+            i = 0;
+        } else {
+            i++;
+        }
+    }, 4000);
+});
+
+$(document).ready(function() {
+
+    // handle Exit Popup
+    var popUp,
+        $body = $(document.body),
+        showed = false;
+
+    popUp = $('.js-seven-years-popup');
+
+    if (popUp.length) {
+        $body.find('.js-seven-years-popup-trigger')
+            .bind('mouseenter', function (event) {
+                posY = event.clientY;
+            })
+
+            .bind('mouseleave', function (event) {
+                if (event.clientY < posY && !showed) {
+                    popUp.show();
+                    showed = true;
+                }
+            });
+
+
+        //Close popup
+        $('.js-seven-years-close-popup').click(function () {
+            popUp.hide();
+        });
     }
+});
 
-    activateYears($('.js-year-text').first());
 
-    $('.js-year').on('click', function(e) {
-        clearInterval(onClickSetTimeoutID);
-        activateYears(this);
+//Expand Sections
 
-    });
+var accordionTab = document.querySelectorAll(".js-seven-years-expand-button");
+var buttons = [accordionTab[0], accordionTab[1]];
 
-    $(function() {
-        var i = 0;
-        var $target = $('.js-year');
-        onClickSetTimeoutID = setInterval(function(){
-            activateYears($target.eq(i).addClass('js-year-selected'));
-            if ( i == $target.length - 1 ) {
-                i = 0;
-            } else {
-                i++;
-            }
-        }, 2000);
+buttons.forEach(function(item){
+    item.addEventListener("click", function(){
+        var expandArea = this.nextElementSibling;
+        var buttonCross = this.childNodes[1];
+        var buttonText = this.childNodes[3];
+        expandArea.classList.toggle("activate-features-expander");
+        buttonCross.classList.toggle("js-seven-years-rotate-cross");
+        buttonText.classList.toggle("js-seven-years-change-color-name");
     });
 });
+
+
+// Team Slider
 
 var divisor = document.getElementById("team-section-divisor"),
     handle = document.getElementById("team-section-handle"),
@@ -57,21 +104,10 @@ $('.team-section-input[type=range]').on('input', function(e){
     });
 }).trigger('input');
 
-    /*var onClickSetTimeoutID;*/
-
 $(window).scroll( function(){
-    /* Check the location of each desired element */
-    $('.js-animated-figure').each( function(i){
-        var top_of_object = $(this).offset().top;
-        var top_of_window = $(window).scrollTop();
-        /* If the object is completely visible in the window, fade it it */
-        if( top_of_window > top_of_object ){
-            $(".js-seven-years-dinamic-figure").fadeIn();
-        }
-    });
+    var top_of_object = $('.js-animated-figure').offset().top;
+    var top_of_window = $(window).scrollTop();
+    if( top_of_window > top_of_object ){
+        $(".js-seven-years-dinamic-figure").fadeIn();
+    }
 });
-
-
-
-
-
--- seven-years-slider.js
+++ seven-years-slider.js
@@ -1,46 +1,4 @@
-
-
-
-/*function activateHexagons(n){
- var currentHexagon = $(n).attr('data-id');
- $target = $('.js-seven-years-slide-pager');
-
- $('.js-seven-years-slide').hide();
- $('.js-seven-years-slide[data-id="'+currentHexagon+'"]').fadeIn();
- }
-
- activateHexagons($('.js-seven-years-slide').first());
-
-
- $(".js-seven-years-left-button").on('click', function(e) {
- clearInterval(onClickSetTimeoutID);
-
- var slideIndex = 1;
- var x = $('.js-seven-years-slide');
- showSlides(slideIndex);
-
- //прописываем какой слайд показывать при загрузке
- function showSlides(n) {
- if (n < 1) {
- slideIndex = x.length
- }  // если slideIndex меньше 1, то slideIndex = кол-ву слайдов (3). То есть, если отображается слайд с индексом 1 (первй слайд), ты кликаешь влево (-1) и значение становится меньше 1, то тебя отправляет на 3 слайд.
-
- if (slideIndex > x.length) {slideIndex = 1}
- }
- });
-
- $(function() {
- var i = 0;
- onClickSetTimeoutID = setInterval(function(){
- $target.removeClass('js-seven-years-slide-pager-selected');
- activateHexagons($target.eq(i).addClass('js-seven-years-slide-pager-selected'));
- if ( i == $target.length - 1 ) {
- i = 0;
- } else {
- i++;
- }
- }, 4000);
- });*/
+// Versions slider
 
 var versionsLeft = document.querySelector("#js-seven-years-left-button");
 var versionsRight = document.querySelector("#js-seven-years-right-button");
